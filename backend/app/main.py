51f1dd07ae30253321c1e0c5ff6d2d5eeb6f35b4
Davidd132 <sreenandpk3@gmail.com>
2026-03-31 20:54:14 +0530

    Added Docker setup, env examples, and startup automation

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,19 +3,26 @@ from app.db.session import engine
 from app.db.base import Base
 from app.api.v1.router import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
+from create_admin import create_admin  
 app = FastAPI(title="Project Management API")
-Base.metadata.create_all(bind=engine)
+@app.on_event("startup")
+def startup_event():
+    print(" Creating tables...")
+    Base.metadata.create_all(bind=engine)
+
+    print(" Creating admin...")
+    create_admin()
 app.include_router(api_router, prefix="/api/v1")
 @app.get("/")
 def test_db():
     try:
         connection = engine.connect()
         return {"message": "DB Connected "}
-    except Exception as e: 
+    except Exception as e:
         return {"error": str(e)}
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
